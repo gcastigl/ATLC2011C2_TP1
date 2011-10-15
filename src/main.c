793365bb626839f345cff0390f4e879ee7e96e73
Dario Susnisky <dario15th@gmail.com>
2011-10-15 19:20:22 -0300

    test para una gramaticaa

--- src/main.c
+++ src/main.c
@@ -7,5 +7,23 @@
 
 int main(int argc, char** argv) {
 
+    struct grammar* grammar = create_grammar();
+    add_non_terminal(grammar, 'A');
+    add_non_terminal(grammar, 'B');
+    add_non_terminal(grammar, 'C');
+    add_terminal(grammar, 'a');
+    add_terminal(grammar, 'b');
+    add_terminal(grammar, 'c');
+    set_distinguished_symbol(grammar, 'A');
+    add_production(grammar, 'A', "aB\0");
+    add_production(grammar, 'B', "aA\0");
+    add_production(grammar, 'A', "c\0");
+    add_production(grammar, 'B', "b\0"); 
+
+    FILE* stream = fopen( "./TEST", "w");
+    grammar_output(stream, grammar);
+    fclose(stream);
+
+    destroy_grammar(grammar);
 }
 
